a19412bc0ae8c487e2cd615fbec579bd7b589069
Gedalya Krycer <gedalya@krycer.com>
2021-01-09 11:18:58 -0800

    Set up initial Sass Files

--- the-shoppies/src/index.js
+++ the-shoppies/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import './sassStyles/_global.scss';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
